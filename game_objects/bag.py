265bdbd7b0360913a2c8f3fcd99fecc1365e8272
Toni Silfver <71206346+tosilfv@users.noreply.github.com>
2026-03-01 11:49:18 +0200

    ul57

--- game_objects/bag.py
+++ game_objects/bag.py
@@ -1,6 +1,8 @@
 """
 Bag item implementation for the Piccolo game.
 """
+from control.mediator import Mediator
+from game_objects.screen import Screen
 
 
 class Bag:
@@ -8,5 +10,12 @@ class Bag:
     Represents a bag item in the game.
     """
 
-    def __init__(self):
+    def __init__(self, screen: Screen, mediator: Mediator | None):
+        self.screen = screen
+        self.mediator = mediator
+
+    def update(self) -> None:
+        pass
+
+    def draw(self) -> None:
         pass
