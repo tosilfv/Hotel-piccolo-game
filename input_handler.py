b81d6b27e4781685fd4a702710d131fb7ad46153
Toni Silfver <71206346+tosilfv@users.noreply.github.com>
2026-04-25 03:34:12 +0300

    ul113

---
+++ input_handler.py
@@ -0,0 +1,65 @@
+"""
+Input handler for separating user input from other game operations.
+"""
+import pygame
+from utils.commands import Command
+
+
+class InputHandler:
+    """
+    Handles user input and translates it into mediator commands.
+
+    Responsibilities:
+        - Capture keyboard input from the user
+        - Map key presses to high-level game commands
+        - Forward commands to the mediator for game object coordination
+        - Decouple input handling from game object logic
+
+    Attributes:
+        mediator: Mediator instance for game internal communication.
+    """
+
+    def __init__(self, mediator):
+        self.mediator = mediator
+
+    def process_input(self) -> None:
+        """
+        Process user input for mediator commands.
+
+        Attributes:
+            keys: Pygame key method.
+        """
+        # Returns a list of boolean values whether each key is pressed or not
+        keys = pygame.key.get_pressed()
+
+        # Left
+        if keys[pygame.K_LEFT]:
+            self.mediator.handle_command(Command.MOVE_LEFT)
+
+        # Right
+        elif keys[pygame.K_RIGHT]:
+            self.mediator.handle_command(Command.MOVE_RIGHT)
+
+        # Up
+        elif keys[pygame.K_UP]:
+            self.mediator.handle_command(Command.ENTER_DOOR)
+
+        # Down
+        elif keys[pygame.K_DOWN]:
+            self.mediator.handle_command(Command.EXIT_DOOR)
+
+        # Jump
+        if keys[pygame.K_SPACE]:
+            self.mediator.handle_command(Command.JUMP)
+
+        # Take trolley
+        if keys[pygame.K_RETURN]:
+            self.mediator.handle_command(Command.TAKE_TROLLEY)
+
+        # Release trolley
+        if keys[pygame.K_RSHIFT]:
+            self.mediator.handle_command(Command.RELEASE_TROLLEY)
+
+        # Not moving reset command
+        if not any(keys):
+            self.mediator.handle_command(Command.STOP)
