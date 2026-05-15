2e3ee42b4b14406416b0d1435a478e05fed84df2
Toni Silfver <71206346+tosilfv@users.noreply.github.com>
2026-05-15 03:52:25 +0300

    ul132

--- control/mediator.py
+++ control/mediator.py
@@ -64,7 +64,7 @@ class Mediator:
             return
 
         # Set and change current scene and background to elevator
-        self.current_scene = RECEPTION
+        self.current_scene = ELEVATOR
         self.background.change_background(ELEVATOR)
         self.audio_manager.stop_music()
 
@@ -200,12 +200,12 @@ class Mediator:
             margin (int): The margin between player and screen edge.
         """
         # Change scene using mediator commands
-        if self.current_scene == RECEPTION:
-            self.handle_command(Command.CHANGE_TO_ELEVATOR)
+        if self.current_scene == ELEVATOR:
+            self.handle_command(Command.CHANGE_TO_RECEPTION)
         elif self.current_scene == ENTRANCE:
             self.handle_command(Command.CHANGE_TO_YARD)
-        elif self.current_scene == ENTRANCE:
-            self.handle_command(Command.CHANGE_TO_RECEPTION)
+        elif self.current_scene == RECEPTION:
+            self.handle_command(Command.CHANGE_TO_ELEVATOR)
         elif self.current_scene == YARD:
             self.handle_command(Command.CHANGE_TO_ENTRANCE)
         else:
