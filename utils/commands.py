ebecfb24640abfb940c02c9c90e8e9e8ee6905ab
Toni Silfver <71206346+tosilfv@users.noreply.github.com>
2026-09-23 08:00:24 +0300

    ul149

--- utils/commands.py
+++ utils/commands.py
@@ -21,6 +21,7 @@ class Command(Enum):
     CHANGE_TO_ELEVATOR = auto()
     CHANGE_TO_ENTRANCE = auto()
     CHANGE_TO_GARAGE = auto()
+    CHANGE_TO_INSIDE_GARAGE = auto()
     CHANGE_TO_LUGGAGE = auto()
     CHANGE_TO_RECEPTION = auto()
     CHANGE_TO_RESTAURANT = auto()
